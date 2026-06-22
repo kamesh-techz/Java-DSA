fd4f374d8e74d586664772a2e85785197e329820
Kamesh S <kameshsj27@gmail.com>
2026-06-22 20:14:10 +0530

    Added Linear Search using predefined array

---
+++ LinearSearchPreDefined.java
@@ -0,0 +1,24 @@
+public class LinearSearchPreDefined{
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50, 60};
+
+        int target = 40;
+
+        boolean found = false;
+
+        for(int i = 0; i < arr.length; i++) {
+
+            if(arr[i] == target) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) {
+            System.out.println("Element not found");
+        }
+    }
+}
