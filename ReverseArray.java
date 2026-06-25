efa1afa903c74ca74f7541dfa2317766b132f4e4
Kamesh S <kameshsj27@gmail.com>
2026-06-25 21:36:45 +0530

    Day 04: Implemented Reverse Array using Predefined

---
+++ ReverseArray.java
@@ -0,0 +1,26 @@
+
+public class ReverseArray {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,2,3,4,5,6,7,8,9,10};
+		int left = 0;
+		int right = arr.length - 1;
+		
+		while(left < right ) {
+			int temp = arr[left];
+			arr[left] = arr[right];
+			arr[right] = temp;
+			
+			left++;
+			right--;
+		}
+		System.out.println("Reversed Array:");
+		
+		for(int val : arr) {
+			System.out.print(val + " ");
+		}
+
+	}
+
+}
