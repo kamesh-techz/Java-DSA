9587c0913c0b9bd2644585ea2642ccbe9e33cf3e
Kamesh S <kameshsj27@gmail.com>
2026-07-08 22:39:26 +0530

    Using reverse variable

---
+++ Strings/Reverse-String/ReverseStringUsingString.java
@@ -0,0 +1,17 @@
+package Strings;
+
+public class ReverseStringUsingString {
+
+	public static void main(String[] args) {
+		
+		String str = "Kamesh";
+		String reverse = "";
+		
+		for(int i = str.length() - 1; i >= 0; i--) {
+			reverse += str.charAt(i);
+		}
+		System.out.println("Original String : " + str);
+		System.out.println("Reversed String : " + reverse);
+	}
+
+}
