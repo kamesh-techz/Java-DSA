695c8f3ca114c3883da91b4a33555a9104d61a5b
Kamesh S <kameshsj27@gmail.com>
2026-07-09 20:37:01 +0530

    Implemented Count Vowels and Consonants

---
+++ Strings/Reverse-String/CountVowel_Consonants.java
@@ -0,0 +1,36 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class CountVowel_Consonants {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a String : ");
+		String str = sc.nextLine();
+		
+		int vowels = 0;
+		int consonants = 0;
+		str = str.toLowerCase();
+		
+		for(int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if(ch >= 'a' && ch <= 'z') {
+				if(ch == 'a' || ch == 'e' || ch =='i' || ch == 'o' || ch == 'u' ) {
+					vowels++;
+				}
+				else {
+					consonants++;
+				}
+				
+			}
+			
+		}
+		System.out.println("Vowels : " + vowels);
+		System.out.println("Consonants : " + consonants);
+		sc.close();
+
+	}
+
+}
